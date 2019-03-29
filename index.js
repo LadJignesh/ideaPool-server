5bc459d6ce3fad0d99d05962e84632a175f9273a
Jignesh Lad <Jignesh.Lad@version1.com>
2019-03-29 11:42:12 +0000

    updated cors method in index

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ var port = process.env.PORT || 3000;
 
 app.use(function (request, response, next) {
     response.header("Access-Control-Allow-Origin", "*");
+    response.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
     response.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
